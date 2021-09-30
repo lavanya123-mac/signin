ccc2fa1180f25820496b9a94f4386851eb147551
lavanya123-mac <lavanyam483@gmail.com>
2021-09-30 11:34:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react';
+
+function App() {
+  return (
+    <div className="App">
+      <AmplifySignOut/>
+    </div>
+  );
+}
+export default withAuthenticator(App)
